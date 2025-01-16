927dd0120a457344dfabda8467537d44e776ae58
zxwanderer <no@mail.com>
2025-01-16 18:48:50 +0300

    updates

--- app/main.js
+++ app/main.js
@@ -1,6 +1,5 @@
 console.log('Hello World from console')
 
-
 const onLoad = () => {
   emu = JSSpeccy(document.getElementById('jsspeccy'), {zoom: 2, sandbox: false, tapeAutoLoadMode: 'usr0'})
   // const emu = JSSpeccy(
