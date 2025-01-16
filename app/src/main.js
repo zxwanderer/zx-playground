5726111ccbccdfaa7d87d466dbc8d2247e4e9c27
zxwanderer <no@mail.com>
2025-01-16 20:10:03 +0300

    some updates

--- app/src/main.js
+++ app/src/main.js
@@ -2,9 +2,5 @@ console.log('Hello World from console')
 
 const onLoad = () => {
   emu = JSSpeccy(document.getElementById('jsspeccy'), {zoom: 2, sandbox: false, tapeAutoLoadMode: 'usr0'})
-  // const emu = JSSpeccy(
-  //   document.getElementById('jsspeccy'),
-  //   { zoom: 2, machine: 128, sandbox: true, autoStart: true, uiEnabled: false, keyboardEnabled: false }
-  // )
 }
 window.addEventListener('load', () => onLoad()) 
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,9 +1,25 @@
 const path = require('path');
+const webpack = require('webpack'); // Добавляем импорт webpack
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/main.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  mode: 'production', // Режим разработки
+  devtool: 'inline-source-map', // Карты исходников для дебага
+  devServer: { // Настройки devServer для HMR
+    static: [
+      './dist',
+      {
+        directory: path.join(__dirname, 'static/jsspeccy'),
+        publicPath: '/jsspeccy',
+      },
+    ],
+    hot: true,
+    open: true,
+    port: 8080,
+  }
+  // watch: true, // Включаем режим слежения за изменениями файлов
 };
